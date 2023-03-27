bce66fe17d51675ec55eb664161a81552bae78a0
Lucas Henrique <lucasmonte2011@gmail.com>
2023-03-27 19:31:31 -0300

    Add new animations to page 04

--- scenes/pag04.lua
+++ scenes/pag04.lua
@@ -10,12 +10,7 @@ local audio = require("audio")
 local scene = composer.newScene()
 
 -- forward declarations and other locals
-local background, buttomLeft, buttomRight, text, baloes, bola, ended, gravar, parar, audioCapture, isRecording, gravando
-
-local linhaBola = nil
-local linhaBaloes = nil
-local linhaBolaPontos = {}
-local linhaBaloesPontos = {}
+local background, buttomLeft, buttomRight, text, baloes, bola, ended, gravar, parar, audioCapture, isRecording, gravando, linhaBola, linhaBaloes, mascaraBola, mascaraBaloes, restart, audioRecorded
 
 local function onButtomLeftTouch( self, event )
 	if event.phase == "ended" or event.phase == "cancelled" then
@@ -38,41 +33,27 @@ end
 
 
 local function animateBalao()
-	transition.to(baloes, { time=10000, x=590, y=display.contentHeight/4, transition=easing.easeOutBounce, 
-			onUpdate=function()
-					table.insert(linhaBaloesPontos, baloes.x + baloes.width/2)
-					table.insert(linhaBaloesPontos, baloes.y + baloes.height/2)
-					linhaBaloes:append(baloes.x + baloes.width/2, baloes.y + baloes.height/2)
-			end
+	transition.to(baloes, { time=10000, x=610, y=display.contentHeight/4, transition=easing.easeOutBounce
+	})
+	transition.to(mascaraBaloes, { time=10000, x=660
 	})
 
-	transition.to(bola, { time=10000, x=590, 
-			onUpdate=function()
-					table.insert(linhaBolaPontos, bola.x + bola.width/2)
-					table.insert(linhaBolaPontos, bola.y + bola.height/2)
-					linhaBola:append(bola.x + bola.width/2, bola.y + bola.height/2)
-			end
+	transition.to(bola, { time=10000, x=610
+	})
+	transition.to(mascaraBola, { time=10000, x=660
 	})
 
 	ended = true
 end
 
 local function onRestart()
-	transition.to(baloes, { time=100, x=display.contentWidth/8, y=display.contentHeight/4, transition=easing.easeOutBounce, 
-			onUpdate=function()
-					linhaBaloes:removeSelf()
-					linhaBaloes = display.newLine( linhaBaloesPontos )
-					linhaBaloes:setStrokeColor( 0, 0, 0 )
-					linhaBaloes.strokeWidth = 5
-			end
+	transition.to(baloes, { time=100, x=display.contentWidth/8, y=display.contentHeight/3, transition=easing.easeOutBounce
 	})
-	transition.to(bola, { time=100, x=display.contentWidth/8, 
-			onUpdate=function()
-					linhaBola:removeSelf()
-					linhaBola = display.newLine( linhaBolaPontos )
-					linhaBola:setStrokeColor( 0, 0, 0 )
-					linhaBola.strokeWidth = 5
-			end
+	transition.to(bola, { time=100, x=display.contentWidth/8
+	})
+	transition.to(mascaraBaloes, { time=100, x=display.contentWidth/8 + 50
+	})
+	transition.to(mascaraBola, { time=100, x=display.contentWidth/8 + 50
 	})
 end
 
@@ -128,37 +109,43 @@ function scene:create( event )
 	buttomLeft.anchorY = 0
 	buttomLeft.x, buttomLeft.y = display.contentWidth/20,  (display.contentHeight - display.contentHeight/6)
 
-  text = display.newImageRect( "images/texto-pag-04.png", display.contentWidth - display.contentWidth/4, display.contentHeight - display.contentHeight/4 )
+  text = display.newImageRect( "images/texto-pag-04-1.png", display.contentWidth - display.contentWidth/4, display.contentHeight - display.contentHeight/4 )
   text.anchorX = 0
   text.anchorY = 0
   text.x, text.y = display.contentWidth/8, 50
 
-	baloes = display.newImageRect( "images/baloes.png", 50, 100 )
+	linhaBaloes = display.newImageRect( "images/curva.png", 485, 60 )
+	linhaBaloes.anchorX = 0
+	linhaBaloes.anchorY = 0
+	linhaBaloes.x, linhaBaloes.y = display.contentWidth/8 + 25, display.contentHeight/3 - 50
+
+	mascaraBaloes = display.newRect( display.contentWidth/8, display.contentHeight/3 - 50, 485, 75 )
+	mascaraBaloes.anchorX = 0
+	mascaraBaloes.anchorY = 0
+	mascaraBaloes.x, mascaraBaloes.y = display.contentWidth/8 + 50, display.contentHeight/3 - 50
+
+	print("Distancia:" .. display.contentHeight/3 - display.contentHeight/4)
+
+	baloes = display.newImageRect( "images/baloes-branco.png", 50, 100 )
 	baloes.anchorX = 0
 	baloes.anchorY = 0
 	baloes.x, baloes.y = display.contentWidth/8, display.contentHeight/3
 
-	bola = display.newImageRect( "images/bola.png", 50, 50 )
+	linhaBola = display.newImageRect( "images/linha-reta.png", 500, 2 )
+	linhaBola.anchorX = 0
+	linhaBola.anchorY = 0
+	linhaBola.x, linhaBola.y = display.contentWidth/8 + 25, display.contentHeight/2.2 + 25
+
+	mascaraBola = display.newRect( display.contentWidth/8, display.contentHeight/2.2 + 25, 500, 5 )
+	mascaraBola.anchorX = 0
+	mascaraBola.anchorY = 0
+	mascaraBola.x, mascaraBola.y = display.contentWidth/8 + 50, display.contentHeight/2.2 + 25
+
+	bola = display.newImageRect( "images/bola-branco.png", 50, 50 )
 	bola.anchorX = 0
 	bola.anchorY = 0
 	bola.x, bola.y = display.contentWidth/8, display.contentHeight/2.2
-
-	-- criar pontos de partida para as linhas
-	linhaBolaPontos = { bola.x + bola.width/2, bola.y + bola.height/2 }
-	linhaBaloesPontos = { baloes.x + baloes.width/2, baloes.y + baloes.height/2 }
-
-	-- criar linha para a trajetória da bola
-	linhaBola = display.newLine(linhaBolaPontos[1], linhaBolaPontos[2], bola.x + bola.width/2, bola.y + bola.height/2)
-	linhaBola:setStrokeColor(1, 1, 1, 0) -- cor vermelha
-	linhaBola.strokeWidth = 50
-	linhaBola:append(bola.x + bola.width/2, bola.y + bola.height/2)
-
-	-- criar linha para a trajetória dos baloes
-	linhaBaloes = display.newLine(linhaBaloesPontos[1], linhaBaloesPontos[2], baloes.x + baloes.width/2, baloes.y + baloes.height/2)
-	linhaBaloes:setStrokeColor(1, 1, 1, 0) -- cor verde
-	linhaBaloes.strokeWidth = 2
-	linhaBaloes:append(baloes.x + baloes.width/2, baloes.y + baloes.height/2)
-
+	
 	gravar = display.newImageRect( "images/gravar.png", 100, 100 )
 	gravar.anchorX = 0
 	gravar.anchorY = 0
@@ -170,16 +157,26 @@ function scene:create( event )
 	gravando.anchorY = 0
 	gravando.x, gravando.y = display.contentWidth/2 - gravando.width/2,  (display.contentHeight - display.contentHeight/20)
 
+	restart = display.newImageRect( "images/restart.png", 100, 100 )
+	restart.anchorX = 0
+	restart.anchorY = 0
+	restart.x, restart.y = display.contentWidth/2 - 50,  (display.contentHeight/4.2)
+	restart.alpha = 0
+
   sceneGroup:insert( background )
 	sceneGroup:insert( buttomRight )
 	sceneGroup:insert( buttomLeft )
   sceneGroup:insert( text )
-	sceneGroup:insert( baloes )
-	sceneGroup:insert( bola )
-	sceneGroup:insert( linhaBola )
 	sceneGroup:insert( linhaBaloes )
+	sceneGroup:insert( baloes )
+	sceneGroup:insert( linhaBola )
+	sceneGroup:insert( bola )
 	sceneGroup:insert( gravar )
 	sceneGroup:insert( gravando )
+	sceneGroup:insert( mascaraBaloes )
+	sceneGroup:insert( mascaraBola )
+	sceneGroup:insert( restart )
+
 end
 
 function scene:show( event )
@@ -204,6 +201,9 @@ function scene:show( event )
 
 		gravar.tap = recordAudio
 		gravar:addEventListener( "tap", gravar )
+
+		restart.tap = onRestart
+		restart:addEventListener( "tap", restart )
 	end
 
 end
@@ -215,11 +215,20 @@ function scene:hide( event )
 	if event.phase == "will" then
 		buttomLeft:removeEventListener( "touch", buttomLeft )
 		buttomRight:removeEventListener( "touch", buttomRight )
+		gravar:removeEventListener( "tap", gravar )
+		restart:removeEventListener( "tap", restart )
 	elseif phase == "did" then
 		-- Called immediately after scene goes off screen.
 		if isRecording then
 			audioCapture:stopRecording()
 		end
+		
+		transition.cancel( baloes )
+		transition.cancel( bola )
+		transition.cancel( mascaraBaloes )
+		transition.cancel( mascaraBola )
+
+		isRecording = false
 	end
 end
 
